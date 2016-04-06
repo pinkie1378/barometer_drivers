38e54f3d15f2c0a9aaa239090b27979a31d432b9
Jeffry Pincus <jeffry.pincus@gmail.com>
2016-04-05 22:11:35 -0400

    Fix typo, remove magic numbers, improve docstrings.

--- abci2cdriver.py
+++ abci2cdriver.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta
 
 
+BITS_IN_BYTE = 8
+
+
 class ABCI2CDriver(object):
     """Base class for writing drivers for I2C sensors."""
     __metaclass__ = ABCMeta
@@ -9,22 +12,23 @@ class ABCI2CDriver(object):
     def array_block_to_value(data_array):
         """
         :param list data_array: List of byte values comprising a sensor
-        measurement in 2's complement format.
+         measurement in 2's complement format.
         :return int: Signed integer value of :attr:`data_array`.
         """
-        assert not [i for i in data_array if i > 255]
+        assert not [i for i in data_array if i >= 2**BITS_IN_BYTE]
+        bits = len(data_array) * BITS_IN_BYTE
         value = 0
         for byte in data_array:
-            value <<= 8
+            value <<= BITS_IN_BYTE
             value |= byte
-        return HP206C.twos_compliment_to_signed_int(value, len(data_array) * 8)
+        return ABCI2CDriver.twos_compliment_to_signed_int(value, bits)
 
     @staticmethod
     def twos_compliment_to_signed_int(twos_compliment_value, bits):
         """From http://stackoverflow.com/a/9147327
 
         :param int twos_compliment_value: 2's compliment unsigned value
-        :param int bits: Size of unsigned_int
+        :param int bits: Number of bits in :attr:`twos_compliment_value`
         :return int: Signed version of :attr:`twos_compliment_value`
         """
         if (twos_compliment_value & (1 << (bits - 1))) != 0:
