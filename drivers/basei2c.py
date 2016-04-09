0ea71af2b74e20b3c802154876332ebf4a2fa627
Jeffry Pincus <jeffry.pincus@gmail.com>
2016-04-09 11:53:08 -0400

    Add bitwise or method and tests, docstrings.

--- drivers/basei2c.py
+++ drivers/basei2c.py
@@ -1,3 +1,6 @@
+import functools
+import itertools
+
 import smbus
 
 
@@ -6,23 +9,38 @@ BITS_IN_BYTE = 8
 
 class BaseI2CDriver(object):
     """Base class for writing drivers for I2C sensors."""
-    def __init__(self, port=1, address):
+
+    def __init__(self, address, port=1):
         self.bus = smbus.SMBus(port)
         self.address = address
 
     def write_byte(self, byte):
-        assert BaseI2CDriver.is_unsigned_byte(i)
+        assert BaseI2CDriver.is_unsigned_byte(byte)
         self.bus.write_byte(self.address, byte)
 
     @staticmethod
+    def do_bitwise_or(val1, val2, *args):
+        """
+        :param int val1: First number.
+        :param int val2: Second number.
+        :param int args: Optional subsequent numbers.
+        :return int: Value from using bitwise or operator on all args.
+        """
+        values = itertools.chain([val1, val2], args)
+        return functools.reduce(lambda x, y: x | y, values)
+
+    @staticmethod
     def is_unsigned_byte(byte):
+        """
+        :param byte: Value to test.
+        :return bool: True if :attr:`byte` contains 8 bits and is non-negative.
+        """
         return 0x00 <= byte <= 0xff
 
     @staticmethod
     def array_block_to_value(data_array):
         """
-        :param list data_array: List of byte values comprising a sensor
-         measurement in 2's complement format.
+        :param list data_array: List of byte values in 2's complement format.
         :return int: Signed integer value of :attr:`data_array`.
         """
         assert not [i for i in data_array
@@ -36,7 +54,7 @@ class BaseI2CDriver(object):
 
     @staticmethod
     def twos_compliment_to_signed_int(twos_compliment_value, bits):
-        """From http://stackoverflow.com/a/9147327
+        """From http://stackoverflow.com/a/9147327.
 
         :param int twos_compliment_value: 2's compliment unsigned value
         :param int bits: Number of bits in :attr:`twos_compliment_value`
--- tests/test_basei2c.py
+++ tests/test_basei2c.py
@@ -3,18 +3,33 @@ import pytest
 from drivers.basei2c import BaseI2CDriver
 
 
-@pytest.mark.parametrize('byte_array, int_value', [
+@pytest.mark.parametrize('byte_array, expected', [
     ([0x00, 0x0a, 0x5c], 2652),
     ([0xff, 0xfc, 0x02], -1022),
     ([0x01, 0x8a, 0x9e], 101022)
 ])
-def test_array_block_to_value(byte_array, int_value):
-    assert BaseI2CDriver.array_block_to_value(byte_array) == int_value
+def test_array_block_to_value(byte_array, expected):
+    assert BaseI2CDriver.array_block_to_value(byte_array) == expected
 
 
-@pytest.mark.parametrize('twos_compliment, bits, int_value', [
+@pytest.mark.parametrize('twos_compliment, bits, expected', [
     (16772216, 24, -5000), (5000, 24, 5000)
 ])
-def test_twos_compliment_to_signed_int(twos_compliment, bits, int_value):
+def test_twos_compliment_to_signed_int(twos_compliment, bits, expected):
     value = BaseI2CDriver.twos_compliment_to_signed_int(twos_compliment, bits)
-    assert value == int_value
+    assert value == expected
+
+
+@pytest.mark.parametrize('byte, expected', [
+    (-0x01, False), (0x00, True), (0x9e, True), (0xff, True), (0x100, False)
+])
+def test_is_unsigned_byte(byte, expected):
+    assert BaseI2CDriver.is_unsigned_byte(byte) == expected
+
+
+@pytest.mark.parametrize('val1, val2, args, expected', [
+    (0b101, 0b010, [], 0b111), (0b101, 0b111, [], 0b111),
+    (0x00, 0x00, [0xec], 0xec), (1, 2, [4, 8, 16], 31)
+])
+def test_do_bitwise_or(val1, val2, args, expected):
+    assert BaseI2CDriver.do_bitwise_or(val1, val2, *args) == expected
